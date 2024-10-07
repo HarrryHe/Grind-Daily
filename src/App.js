db68dd02f11ba1f0906272c45864524e890a6d24
Jiacheng He <tuo44998@temple.edu>
2024-10-07 04:19:38 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
+import axios from 'axios'; // 导入 axios
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const [tasks, setTasks] = useState([]); // 创建一个状态来存储任务
+
+  useEffect(() => {
+    // 使用 Axios 发起 GET 请求，获取任务列表
+    axios.get('http://localhost:8080/api/tasks')
+      .then(response => {
+        setTasks(response.data); // 将获取到的数据设置到 tasks 状态中
+      })
+      .catch(error => {
+        console.error("There was an error fetching the tasks!", error);
+      });
+  }, []); // 空依赖数组表示只在组件加载时执行一次
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +33,18 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+        <h2>Task List</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
